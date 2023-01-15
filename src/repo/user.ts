f2f6b01b017228818ef55bc4bae53e4b9a77dd5b
Sergey <sergey@siemonster.com>
2023-01-15 11:15:58 +0300

    added postgres lib, user functions, docker-compose

--- src/repo/user.ts
+++ src/repo/user.ts
@@ -1,19 +1,46 @@
 import { IUser } from "../model/user";
+import { Client } from 'pg'
 
 export class UserRepo {
-    public async createUser(user: IUser) {
+    constructor(
+        private pgClient: Client,
+    ) {}
 
+    public async createUser(user: IUser) {
+        const query = `
+        INSERT INTO users
+            (email, passwordHash, name)
+        VALUES 
+            (${user.email}, ${user.passwordHash}, ${user.name});
+        `
+
+        await this.pgClient.query(query)
     }
 
     public async updateUser(user: IUser) {
-
+        const query = `
+        UPDATE users SET 
+        email = ${user.name}. passwordHash = ${user.email}, name = ${user.name}
+        WHERE id = ${user.id};
+        `
+        
+        await this.pgClient.query(query)
     }
 
     public async deleteUser(id: string) {
+        const query = `
+        DELETE FROM users WHERE id = ${id};
+        `
 
+        await this.pgClient.query(query)
     }
 
     public async readUser(id: string) {
+        const query = `
+        SELECT * FROM users WHERE id = ${id};
+        `
 
+        const result = await this.pgClient.query(query)
+        return result.rows[0] ?? null
     }
 }
